deb6a27fea7a2f80dbabefbe84ca1e4e99a4482b
Huy Ly <huylg2109@gmail.com>
2022-03-17 19:59:18 +0700

    delete comment in main.py

--- main.py
+++ main.py
@@ -7,15 +7,6 @@ from search import Solver
 def main():
     manager= BlockManager(STATES)
     solver = Solver(manager.all_block)
-    # goal_node = solver.bfs()
-    # manager.goal_state = goal_node.state
-
-    # s = f""
-    # for j in manager.goal_state:
-    #     s += f"{j.start_pos}:{j.width}x{j.height} --->"
-    # print(s)
-    # print(solver.step)
-
     ui = GUI(manager, solver)
 
 
